5e68f9dc8c8fa39c6bf98f470c48c3c45bafb347
Jiaozhiye <jiaozhiye@genolo.cn>
2019-04-04 09:16:44 +0800

    修复上传功能bug

--- server/app/service/upload.js
+++ server/app/service/upload.js
@@ -8,17 +8,24 @@ const Service = require('egg').Service;
 class UploadService extends Service {
   async doUpload(files, dirname) {
     const { ctx } = this;
+    const dirpath = path.join(__dirname, '..', `upload/${dirname}`);
     for (const file of files) {
-      // console.log(file);
-      const filepath = `upload/${dirname}/${path.basename(file.filepath)}`;
+      // 文件名
+      const filename = path.basename(file.filepath);
+      // 文件静态访问路径
+      const file_static_path = `upload/${dirname}/${filename}`;
       try {
-        // 文件移动
-        await fs.rename(file.filepath, path.join(__dirname, '..', filepath));
+        // 如果目标文件夹不存在，创建之
+        if (!fs.existsSync(dirpath)) {
+          fs.mkdirSync(dirpath);
+        }
+        // 文件从 TEMP 临时文件夹移动到目标文件夹
+        await fs.rename(file.filepath, path.join(dirpath, filename));
         ctx.body = {
           code: 1,
           data: {
-            path: filepath,
-            url: `http://${ctx.header.host}/${filepath}`
+            path: file_static_path,
+            url: `http://${ctx.header.host}/${file_static_path}`
           }
         };
       } catch (e) {
